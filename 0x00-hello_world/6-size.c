a4d3ef2f0f338f78f212fcb0f7f7bb5366eec835
Dennis <deniskiprono53@gmail.com>
2023-03-17 21:36:27 -0700

    6-size.c

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+/**
+ * main- entry pointDes
+ * Description: print sizes ending on anew line
+ *
+ * Return: 0
+*/
+int main(void)
+
+{
+	char a;
+	int b;
+	long int c;
+	long long int d;
+	float f;
+
+	printf("size of a char: %lu byte(s)\n", (unsigned long)sizeof(a));
+	printf("size of an int: %lu byte(s)\n", (unsigned int)sizeof(b));
+	printf("size of a long int: %lu byte(s)\n", (unsigned long)sizeof(c));
+	printf("size of long long int: %lu byte(s)\n", (unsigned long)sizeof(d));
+	printf("size of float: %lu byte(s)\n", (unsigned int)sizeof(f));
+return (0);
+}
