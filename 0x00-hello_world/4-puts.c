343c70c6cd5ec39fc3c9315274f2280ab8ffb9e1
Dennis <deniskiprono53@gmail.com>
2023-03-17 19:07:44 -0700

    My first C commit

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,17 +1,12 @@
 #include<stdio.h>
-
 /**
- * main- Entry
- *Description: 'The program description'
- *Parameter: Program prints aline using puts function
- * Return: Always 0 (success)
+ * main- entry point
+ * Description: print a new line using puts
+ *
 */
-
 int main(void)
+
 {
-	/*printing using puts*/
-	char mystr = "\"Programming is like building a multilingual puzzle"
- ;
-	puts(mystr); /*writing the string to stdout*/
-return: 0;
+	puts("\"Programming is like building a multilingual puzzle");
+return(0);
 }
