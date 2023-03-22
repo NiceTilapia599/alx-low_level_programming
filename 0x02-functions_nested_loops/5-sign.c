aa251b96aa9d5e74517660de031d50937afe6cc0
Dennis <deniskiprono53@gmail.com>
2023-03-22 08:50:30 -0700

    signs

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -14,17 +14,17 @@ int print_sign(int n)
 {
 	if (n > 0)
 	{
-		_putchar('+');
+		_putchar(43);
 		return (1);
 	}
 	else if (n == 0)
 	{
-		_putchar('0');
+		_putchar(45);
 		return (0);
 	}
 	else
 	{
-		_putchar('_');
+		_putchar(48);
 		return (-1);
 	}
 }
