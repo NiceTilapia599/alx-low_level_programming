2901dc1429977e8cc7d3e66f81c934602bd207b2
Dennis <deniskiprono53@gmail.com>
2023-03-24 23:18:40 -0700

    change mode .c to .s

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -14,10 +14,10 @@ int main(void)
 	long long int d;
 	float f;
 
-	printf("Size char: %lu byte(s)\n", (char)sizeof(a));
-	printf("Size of an int: %lu byte(s)\n", (int)sizeof(b));
-	printf("Size of long int: %lu byte(s)\n", (long int)sizeof(c));
-	printf("Size of long long int: %lu byte(s)\n", (long long int)sizeof(d));
-	printf("Size of float: %lu byte(s)\n", (float)sizeof(f));
+	printf("Size a  char: %lu byte(s)\n", (char)sizeof(char));
+	printf("Size of an int: %lu byte(s)\n", (int)sizeof(int));
+	printf("Size of a long int: %lu byte(s)\n", (long int)sizeof(long int));
+	printf("Size of a long long int: %lu byte(s)\n", (long long int)sizeof(long long int));
+	printf("Size of a float: %lu byte(s)\n", (float)sizeof(float));
 	return (0);
 }
